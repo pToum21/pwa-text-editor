661cc21a21a2032e24494a252b7fa257d4f9b47a
toum21 <pey2moo1@gmail.com>
2024-01-04 12:45:13 -0800

    updated putDb function inside of database.js, now it must be tested

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,9 +17,16 @@ export const putDb = async (content) => {
   console.log('Add to database');
   // create connection
   const jatedb = await openDB('jate', 1);
+  const tx = jatedb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add({ jate: content });
+  const result = await request;
 
-
-  console.error('putDb not implemented')
+  if (result) {
+    console.log('Data saved to the database', result);
+  } else {
+    console.error('putDb not implemented')
+  }
 };
 
 // TODO: Add logic for a method that gets all the content from the database
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -8,7 +8,8 @@ window.addEventListener('beforeinstallprompt', (event) => {
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {const promptEvent = window.deferredPrompt;
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
 
     if (!promptEvent) {
      return;
