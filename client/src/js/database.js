63c083468c2455c9ca8722aa950c74fb985109a2
toum21 <pey2moo1@gmail.com>
2024-01-04 12:46:56 -0800

    added comments inside of putDb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,11 +17,16 @@ export const putDb = async (content) => {
   console.log('Add to database');
   // create connection
   const jatedb = await openDB('jate', 1);
+  // creates transaction
   const tx = jatedb.transaction('jate', 'readwrite');
+  // stores the object
   const store = tx.objectStore('jate');
+  // handles the request
   const request = store.add({ jate: content });
+  // awaits the result
   const result = await request;
 
+  // checks result
   if (result) {
     console.log('Data saved to the database', result);
   } else {
