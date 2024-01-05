8d1d75db6b8982ee6d4b94475b3a25a12e098a1a
toum21 <pey2moo1@gmail.com>
2024-01-04 19:24:24 -0800

    added image loader in

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -45,10 +45,17 @@ module.exports = () => {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', 
-              '@babel/transform-runtime'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread',
+                '@babel/transform-runtime'],
             },
           },
+        },
+        {
+
+          //Image loader
+          test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+          type: "asset/resource",
+
         },
       ],
     },
