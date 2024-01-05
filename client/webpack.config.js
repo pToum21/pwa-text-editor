2dc4ddc321914458cb7e8218bb372bb031ab8c69
toum21 <pey2moo1@gmail.com>
2024-01-04 21:04:37 -0800

    updated the webpack inject and webpackpwamanifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -23,13 +20,25 @@ module.exports = () => {
         title: "JATE",
       }),
 
-      // new InjectManifest({
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
 
-      // }),
-
-      // new WebpackPwaManifest({
-
-      // })
+      new WebpackPwaManifest({
+        name: "JUST ANOTHER TEXT EDITOR",
+        short_name: "J.A.T.E",
+        description: "A text editor that also allows you to take notes with the help of webpack",
+        publicPath: "./",
+        start_url: "./",
+        icons: [
+          {
+            src: path.resolve('./src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          }
+        ]
+      })
     ],
 
     module: {
@@ -51,7 +60,6 @@ module.exports = () => {
           },
         },
         {
-
           //Image loader
           test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
           type: "asset/resource",
