8ea4891da80c34f906a4ee89d4858ea6413f2a41
toum21 <pey2moo1@gmail.com>
2024-01-08 21:35:35 -0800

    format update

--- client/src-sw.js
+++ client/src-sw.js
@@ -25,8 +25,6 @@ warmStrategyCache({
 });
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
-
-
 registerRoute(
   ({ request }) => ['script', 'style', 'worker'].includes(request.destination),
   new StaleWhileRevalidate({  
